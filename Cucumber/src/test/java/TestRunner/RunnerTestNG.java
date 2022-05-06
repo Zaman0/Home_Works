4b9eb0ab1c85e6d1bad977efa7492f9546ca2ded
Shah <93167568+Zaman0@users.noreply.github.com>
2022-05-05 23:44:11 -0400

    Add files via upload

--- Cucumber/src/test/java/TestRunner/RunnerTestNG.java
+++ Cucumber/src/test/java/TestRunner/RunnerTestNG.java
@@ -4,8 +4,10 @@ package TestRunner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions (features = "D:\\Work\\Java_Prac\\Cucumber\\src\\resources\\java\\Features\\Login.feature",
-glue="StepDefination") // Package Name
+@CucumberOptions (features = "D:\\Work\\Java_Prac\\Cucumber\\src\\resources\\java\\Features\\Loginbak.feature",
+
+glue="StepDefination", monochrome = true)
+
 public class RunnerTestNG extends AbstractTestNGCucumberTests {
 
 }
